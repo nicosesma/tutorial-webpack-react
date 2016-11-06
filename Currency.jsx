888d853ac97f20e0b5780d52dfb81586f40d9ce6
nicosesma <nicosm310@gmail.com>
2016-11-05 21:59:23 -0700

    setting up api countries call

---
+++ Currency.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import $ from 'jquery'
+
+class Currency extends Component {
+  render() {
+    return <h1>World Currency</h1>
+  }
+}
+
+ReactDOM.render(<Currency/>, document.getElementById('currency') )
